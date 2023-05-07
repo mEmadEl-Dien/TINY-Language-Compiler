2ded45e92a1fd7f1ef8eb642fbecea393c84c2b0
Mohamed Emad El-Dien Mahmoud <85120725+mEmadEl-Dien@users.noreply.github.com>
2023-05-07 12:50:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,18 @@
 
 int main()
 {
-    //Scanner scanner("read x;");
-    //Parser parser(scanner);
-    //parser.Parse();
-    //cout<<parser.root->children[0]->token.type<<endl;
+    Scanner scanner("read x;");
+    Parser parser(scanner);
+    parser.Parse();
+    cout<<parser.root->children[0]->token.type<<endl;
 
-    //Token token = scanner.Get_Next_Token();
+    Token token = scanner.Get_Next_Token();
 
-    //while(token.type != ENDOFFILE)
-    //{
-        //cout << token.type << " " << token.value << endl;
-        //token = scanner.Get_Next_Token();
-    //}
-
-    cout << "helloWorld" << endl;
+    while(token.type != ENDOFFILE)
+    {
+        cout << token.type << " " << token.value << endl;
+        token = scanner.Get_Next_Token();
+    }
 
     return 0;
 }
